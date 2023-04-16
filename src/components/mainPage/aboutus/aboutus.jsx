1e92d557507091a7720b30babed93acd99f9f1f9
Samaresh Samanta <ultragamer046@gmail.com>
2023-04-17 03:29:39 +0530

    Add: about us section done

--- src/components/mainPage/aboutus/aboutus.jsx
+++ src/components/mainPage/aboutus/aboutus.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import classes from './aboutus.module.css'
 
+import img3 from '../../../assets/img3.svg'
+
 export const Aboutus = () => {
     return (
         <div className={classes.majorContainer}>
@@ -10,7 +12,15 @@ export const Aboutus = () => {
                 </p>
             </div>
             <div className={classes.main}>
+                <div className={classes.adjust}>
 
+                </div>
+                <img src={img3}/>
+                <div className={classes.forBlur}>
+                    <p className={classes.quote}>“ At Toronto Water Taxi, your safety is our top priority. We adhere to strict safety protocols, including routine vessel maintenance, regular safety checks, and highly trained and experienced captains.“</p>
+                    <p className={classes.name}>Mr. Andrew Samuel</p>
+                    <p className={classes.pos}>Managing director (MD)</p>
+                </div>
             </div>
         </div>
     )
