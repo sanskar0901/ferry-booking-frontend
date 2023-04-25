4ea0e385a72935ccc412219fc36e275728f179d8
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2023-04-25 19:17:01 +0530

    URL querry error fixed

---
+++ src/components/mainPage/Mainpage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Aboutus } from './aboutus/aboutus'
+import { Header } from './header/header'
+import BookingWrapper from './bookingSection/booking'
+import { FooterTop } from './footer/footer'
+import { Intro } from './intro/intro'
+
+function Mainpage() {
+  return (
+    <div>
+        <Header/>
+        <Intro/>
+        <Aboutus/>
+        <BookingWrapper/>
+        <FooterTop/>
+    </div>
+  )
+}
+
+export default Mainpage
