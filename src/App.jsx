95a1aabb1ecdc045836ecaf662537b8a260a56c8
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2023-06-17 23:11:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-
+import { Adminpage } from './components/adminPage/Adminpage'
 import Mainpage from './components/mainPage/Mainpage';
 
 function App() {
